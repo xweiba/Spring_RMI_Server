5602d7f5455e57b270c5c3f47569142c85a8c520
xweiba <lh1738289913@gmail.com>
2018-06-08 00:33:45 +0800

    去掉多余修饰符

--- src/main/java/com/jnshu/service/StudentService.java
+++ src/main/java/com/jnshu/service/StudentService.java
@@ -5,7 +5,7 @@ import com.jnshu.model.Student;
 import java.util.List;
 
 public interface StudentService {
-    public List<Student> getRencentStudent(int count);
-    public void saveStudent(Student student);
-    public Student getStudent(long id);
+    List<Student> getRencentStudent(int count);
+    void saveStudent(Student student);
+    Student getStudent(long id);
 }
