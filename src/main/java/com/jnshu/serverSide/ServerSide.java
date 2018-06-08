c669008a6fd87666a7faf751e622a914d58cd507
xweiba <lh1738289913@gmail.com>
2018-06-09 02:31:01 +0800

    测试

--- src/main/java/com/jnshu/serverSide/ServerSide.java
+++ src/main/java/com/jnshu/serverSide/ServerSide.java
@@ -3,6 +3,8 @@ package com.jnshu.serverSide;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.net.UnknownHostException;
+
 /**
  * @program: Srping_RMI
  * @description: 服务器端
@@ -11,7 +13,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  **/
 
 public class ServerSide {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException {
+        System.setProperty("java.rmi.server.hostname", "192.168.0.115");
+        System.out.println(java.net.InetAddress.getLocalHost().getHostAddress());
         ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:applicationContext-SMI-Server.xml");
     }
 }
